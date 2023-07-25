4bdb2f054bd7e451add8a034d6dd825f50b91032
Rizki <rizkidliyaulhaq22@gmail.com>
2023-07-26 00:48:33 +0700

    change login desain

--- src/scripts/views/login/pages/main-login.js
+++ src/scripts/views/login/pages/main-login.js
@@ -15,8 +15,13 @@ const mainLogin = {
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <br>
+                        <br>
+                            <div class="col-lg-6 d-none d-lg-block"><img src ="img/logo.png" style="display: block; margin-left: 5%; margin-right: auto; width: 100%; margin-top: 12%; margin-bottom: 12%;"></div>
                             <div class="col-lg-6">
+                            <br>
+                            <br>
+                            <br>
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
@@ -31,23 +36,13 @@ const mainLogin = {
                                             <input type="password" class="form-control form-control-user"
                                                 id="InputPassword" placeholder="Password">
                                         </div>
-                                        <button type='submit' class="btn btn-primary btn-user btn-block">
+                                        <button type='submit' class="btn btn-primary btn-user btn-block" style="background-color:#302c34;">
                                             Login
                                         </button>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
