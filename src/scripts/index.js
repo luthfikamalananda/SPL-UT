18d4ecc916a77289306945976d2134b60084a6d7
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-07-27 00:06:23 +0700

    calling role in database and cannot access admin page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime';
 import AppHome from './views/home/app';
 import firebaseConfig from './globals/firebaseConfig';
 import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile  } from "firebase/auth";
 // import swRegister from './utils/sw-register';
 // import 'lazysizes';
@@ -13,12 +14,13 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, updat
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 const appHome = new AppHome({
   maincontent: document.querySelector('#mainhome')
 });
 
-onAuthStateChanged(auth, (userCredential) => {
+onAuthStateChanged(auth, async (userCredential) => {
   console.log(userCredential);
   if (userCredential) {
     const uid = userCredential.uid;
@@ -28,6 +30,18 @@ onAuthStateChanged(auth, (userCredential) => {
     btnLogout.addEventListener('click', () => {
         signOut(auth)
     })
+
+    const docRef = doc(db, "user", uid);
+    const docSnap = await getDoc(docRef);
+    const role = docSnap.data().role
+    console.log(role);
+
+    // if (role == 'department head') {
+    //   const adminPage = document.getElementById('adminPage');
+    //   adminPage.style.display = 'none'
+    //   console.log('rtse');
+    // }
+
 
     const userName = document.getElementById('userName');
     userName.innerText = userCredential.displayName
