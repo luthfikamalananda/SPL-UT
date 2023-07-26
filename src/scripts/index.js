242dbadc04d6e93a19e91fee5a4e1a4e0d2d59f9
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-07-26 20:56:29 +0700

    displaying name in navbar after log in

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import 'regenerator-runtime';
 import AppHome from './views/home/app';
 import firebaseConfig from './globals/firebaseConfig';
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile  } from "firebase/auth";
 // import swRegister from './utils/sw-register';
 // import 'lazysizes';
 // import 'lazysizes/plugins/parent-fit/ls.parent-fit';
@@ -10,33 +10,9 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from
 // const START = 10;
 // const NUMBER_OF_IMAGES = 100;
 
-
-// // Authentication
-// const navFavourite = document.getElementById('favouriteNavLI');
-// const navReview = document.getElementById('reviewedNavLI');
-// const navProfile = document.getElementById('profileNav');
-// if(localStorage.getItem('user')) {
-//   const user = localStorage.getItem('user');
-//   const data = JSON.parse(user)
-//   document.getElementById('profileNav').innerHTML = ` ${data.nama.split(' ')[0]} <img src="assets/images/profile-header.jpg" alt="">`;
-// } else if (localStorage.getItem('moderator')) {
-//   const mod = localStorage.getItem('moderator');
-//   const data = JSON.parse(mod)
-//   document.getElementById('profileNav').innerHTML = ` ${data.nama.split(' ')[0]} <img src="assets/images/profile-header.jpg" alt="">`;
-//   navFavourite.setAttribute('style', 'display: none;')
-//   navReview.setAttribute('style', 'display: none;')
-//   navProfile.setAttribute('href', '#/profilemod')
-// } else {
-//   navFavourite.setAttribute('style', 'display: none;')
-//   navReview.setAttribute('style', 'display: none;')
-//   navProfile.setAttribute('href', '/login')
-// }
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-console.log('auth:', auth.currentUser);
-
 
 const appHome = new AppHome({
   maincontent: document.querySelector('#mainhome')
@@ -47,12 +23,14 @@ onAuthStateChanged(auth, (userCredential) => {
   if (userCredential) {
     const uid = userCredential.uid;
     console.log(uid);
-    // ...
     
     const btnLogout = document.getElementById('btnLogout');
     btnLogout.addEventListener('click', () => {
         signOut(auth)
     })
+
+    const userName = document.getElementById('userName');
+    userName.innerText = userCredential.displayName
 
   } else {
     window.location.href = '/login';
@@ -67,30 +45,3 @@ window.addEventListener('load', () => {
   appHome.renderPage();
   // swRegister();
 });
-
-
-// // Search Function
-// const searchText = document.getElementById('searchText');
-// searchText.addEventListener('keydown', (e) => {
-//   // console.log(e);
-//   if (e.keyCode == 13)  {
-//     searchText.value;
-//     e.preventDefault()
-//     // console.log(searchText.value);
-//     window.location.href = `#/search/${searchText.value}`
-//   }
-// })
-
-// // Navbar Hamburger
-// const hamburgerButton = document.getElementById('hamburger');
-// const navbarContents = document.getElementById('navbar');
-// hamburgerButton.addEventListener('click', (e) => {
-//   const hamburgerAtrributes = hamburgerButton.getAttribute('class')
-//   // console.log(hamburgerAtrributes);
-//   hamburgerButton.setAttribute('class', 'menu-trigger active')
-//   navbarContents.setAttribute('style', 'display: block;')
-//   if (hamburgerAtrributes == 'menu-trigger active') {
-//     hamburgerButton.setAttribute('class', 'menu-trigger')
-//     navbarContents.setAttribute('style', 'display: none;')
-//   }
-// })
