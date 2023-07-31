fcd6664cf6ce7451207dd2e93e610e654ef19db8
Luthfi <luthfi_kamal_ananda@yahoo.com>
2023-08-01 02:14:04 +0700

    add register function and read user data

--- src/scripts/views/home/pages/adminpage.js
+++ src/scripts/views/home/pages/adminpage.js
@@ -1,6 +1,7 @@
 import firebaseConfig from "../../../globals/firebaseConfig";
 import { initializeApp } from "firebase/app";
-import { collection, getFirestore, query, where, getDocs } from "firebase/firestore";
+import { collection, getFirestore, query, where, getDocs, setDoc, doc } from "firebase/firestore";
+import { nanoid, customAlphabet } from "nanoid";
 
 const adminPage = {
     async render(){
@@ -28,101 +29,50 @@ const adminPage = {
                         </tr>
                     </thead>
                     <tbody id='bodyTable'>
-                        <tr>
-                            <td>Thomas Hardy</td>
-                            <td>thomashardy@mail.com</td>
-                            <td>89 Chiaroscuro Rd, Portland, USA</td>
-                            <td>(171) 555-2222</td>
-                            <td>
-                                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Dominique Perrier</td>
-                            <td>dominiqueperrier@mail.com</td>
-                            <td>Obere Str. 57, Berlin, Germany</td>
-                            <td>(313) 555-5735</td>
-                            <td>
-                                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Maria Anders</td>
-                            <td>mariaanders@mail.com</td>
-                            <td>25, rue Lauriston, Paris, France</td>
-                            <td>(503) 555-9931</td>
-                            <td>
-                                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Fran Wilson</td>
-                            <td>franwilson@mail.com</td>
-                            <td>C/ Araquil, 67, Madrid, Spain</td>
-                            <td>(204) 619-5731</td>
-                            <td>
-                                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                            </td>
-                        </tr>					
-                        <tr>
-                            <td>Martin Blank</td>
-                            <td>martinblank@mail.com</td>
-                            <td>Via Monte Bianco 34, Turin, Italy</td>
-                            <td>(480) 631-2097</td>
-                            <td>
-                                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                            </td>
-                        </tr> 
+                       
                     </tbody>
                 </table>
             </div>
         </div>        
     </div>
-    <!-- Edit Modal HTML -->
+    <!-- Add Modal HTML -->
     <div id="addEmployeeModal" class="modal fade">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form>
+                <form id='formAddUser' action="#" method='post'>
                     <div class="modal-header">						
                         <h4 class="modal-title">Add Employee</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     </div>
                     <div class="modal-body">					
                         <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" class="form-control" required>
+                            <label for="nameInput">Name</label>
+                            <input type="text" class="form-control" required id='nameInput' name='nameInput'>
                         </div>
                         <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" class="form-control" required>
+                            <label for='emailInput'>Email</label>
+                            <input type="email" class="form-control" required id='emailInput' name='emailInput'>
                         </div>
                         <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" required>
+                            <label for='passwordInput'>Password</label>
+                            <input type="password" class="form-control" required id='passwordInput' name='passwordInput'>
                         </div>
                         <div class="form-group">
-                        <label>Role </label>
-                                <form>
-                                    <select id="role" class="form-control">
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <option value="1"><a class="dropdown-item">Departemen Head</a></option>
-                                            <option value="2"><a class="dropdown-item">HCBC</a></option>
-                                            <option value="3"><a class="dropdown-item">General Marketing</a></option>
-                                            <option value="3"><a class="dropdown-item">Departement Head Human Capital</a></option>
-                                        </div>
-                                    </select>
-                                </form>
-                            </div>
+                        <label for='roleInput'>Role </label>
+                                <select class="form-control" id='roleInput' name='roleInput'>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <option value="karyawan"><a class="dropdown-item">Karyawan</a></option>
+                                        <option value="departemen_head"><a class="dropdown-item">Departemen Head</a></option>
+                                        <option value="hcbc"><a class="dropdown-item">HCBC</a></option>
+                                        <option value="general_marketing"><a class="dropdown-item">General Marketing</a></option>
+                                        <option value="departement_head_human_capital"><a class="dropdown-item">Departement Head Human Capital</a></option>
+                                    </div>
+                                </select>
                         </div>					
                     </div>
                     <div class="modal-footer">
                         <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-                        <input type="submit" class="btn btn-success" value="Add">
+                        <button type="submit" class="btn btn-success" id='btnAdd'>Add</button>
                     </div>
                 </form>
             </div>
@@ -152,13 +102,13 @@ const adminPage = {
                 </div>
                 <div class="form-group">
                 <label>Role </label>
-                        <form>
-                            <select id="role" class="form-control">
+                            <select class="form-control">
                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <option value="1"><a class="dropdown-item">Departemen Head</a></option>
-                                    <option value="2"><a class="dropdown-item">HCBC</a></option>
-                                    <option value="3"><a class="dropdown-item">General Marketing</a></option>
-                                    <option value="3"><a class="dropdown-item">Departement Head Human Capital</a></option>
+                                    <option value="karyawan"><a class="dropdown-item">Karyawan</a></option>
+                                    <option value="departemen_head"><a class="dropdown-item">Departemen Head</a></option>
+                                    <option value="hcbc"><a class="dropdown-item">HCBC</a></option>
+                                    <option value="general_marketing"><a class="dropdown-item">General Marketing</a></option>
+                                    <option value="departement_head_human_capital"><a class="dropdown-item">Departement Head Human Capital</a></option>
                                 </div>
                             </select>
                         </form>
@@ -197,26 +147,82 @@ const adminPage = {
     },
 
     async afterRender(){
-        $(document).ready(function() {
-            let table = $('#dataTable').DataTable({
-                "columnDefs": [
-                    { "width": "20%", "targets": 0}
-                ],
-                 'order': [[1, 'asc']]
-            });
-          });
-
+        // Initialize Database
         const app = initializeApp(firebaseConfig)
         const db = getFirestore(app)
 
+        // Read All User
+        const bodyTable = document.getElementById('bodyTable');
         const initializeData = query(collection(db, "user"), where("role", "!=", "admin"))
         const userData = await getDocs(initializeData)
         userData.forEach(user => {
             console.log(user.id, '=>', user.data());
+            bodyTable.innerHTML += `<tr>
+            <td>${user.data().name}</td>
+            <td>${user.data().email}</td>
+            <td style='text-transform: capitalize;'>${user.data().role}</td>
+            <td>${user.data().last_login}</td>
+            <td>
+                <a href="#editEmployeeModal" data-id='${user.id}' class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                <a href="#deleteEmployeeModal" data-id='${user.id}' class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+            </td>
+        </tr>`
         });
 
-          
-        
+        // Register
+        const name = document.getElementById('nameInput');
+        const email = document.getElementById('emailInput');
+        const password = document.getElementById('passwordInput');
+        const role = document.getElementById('roleInput');
+        const formAddUser = document.getElementById('formAddUser');
+
+        formAddUser.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const nanoid = customAlphabet('1234567890', 10)
+            const data = {
+                name: name.value,
+                email: email.value,
+                password: password.value,
+                role: role.value,
+                last_login: 'Belum Login'
+            }
+            if (role.value == 'karyawan') {
+                data.jam_lembur = 120
+            }
+            try {
+                await setDoc(doc(db, "user", `${role.value}_${nanoid()}`), data)
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Penambahan Berhasil',
+                    text: 'Selamat akun yang anda tambahkan dapat digunakan',
+                    showCloseButton: true,
+                    }).then((result) => {
+                        /* Read more about isConfirmed, isDenied below */
+                        if (result.isConfirmed) {
+                        location.reload();
+                        } 
+                    })
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Penambahan Gagal',
+                    text: error,
+                    showCloseButton: true,
+                    allowOutsideClick: false
+                    })
+            }
+        });
+
+        // Data Tables
+        $(document).ready(function() {
+            let table = $('#dataTable').DataTable({
+                retrieve: true,
+                "columnDefs": [
+                    { "width": "20%", "targets": 0}
+                ],
+                 'order': [[1, 'asc']],
+            });
+        });
     }
 }
 export default adminPage;
